cccfeec604d94736c05b29a055a4836d5b619f92
pino18 <jpinor@unal.edu.co>
2022-11-04 19:27:28 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,58 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import re
 import pandas as pd
+from datetime import datetime
 
+df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+
+#print(df.tail())
+
+#print(df.sexo.value_counts())
+#print(df.tipo_de_emprendimiento.value_counts())
+
+
+#print(df.idea_negocio.value_counts())
+#print(df.línea_credito.value_counts())
+#print(df.barrio.value_counts())
+
+print("--------------------------------------------------------")
 
 def clean_data():
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[columna] = df[columna].str.lower()
+        
+    for columna in ['idea_negocio', 'línea_credito', 'barrio']:
+        df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
+        df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
 
-    #
-    # Inserte su código aquí
-    #
+
+    for i in ["\$[\s*]",",","\.00"]:
+        df['monto_del_credito'] = df['monto_del_credito'].str.replace(i, "")
+
+    #df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
+    #df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
+    #df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
+    
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+
+    df.drop_duplicates(inplace = True)
 
     return df
+
+#df1=clean_data()
+#print(df.sexo.value_counts())
+#print(df.tipo_de_emprendimiento.value_counts())
+#print(df.idea_negocio.value_counts())
+#print(df.línea_credito.value_counts())
+#print(df.barrio.value_counts())
+
+
+
